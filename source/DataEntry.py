18d3e716e9e974c4735d3c7a7905f691f943b0bb
dahlen <marcin.dahlen@gmail.com>
2020-12-24 02:07:26 +0100

    optiparm work in progress

--- source/DataEntry.py
+++ source/DataEntry.py
@@ -1,6 +1,6 @@
 class DataEntry(object):
     def __init__(self, order_no, pd_value, into_default_flag):
-        self.order_no = order_no
-        self.pd_value = pd_value
-        self.into_default_flag = into_default_flag
+        self.order_no = int(order_no) if type(order_no) is int else order_no
+        self.pd_value = float(pd_value) if type(pd_value) is float else pd_value
+        self.into_default_flag = int(into_default_flag) if type(into_default_flag) is int else into_default_flag
         self.dummy_value = None
--- source/OptiparmBasic.py
+++ source/OptiparmBasic.py
@@ -1,18 +1,26 @@
-from utils import variables
+import math
 
+from utils import variables
+from source.DataStorage import DataStorage
 
 class Optiparm(object):
     def __init__(self):
-        self.results = [[] for k in variables.classes_no]
-
-    def sort_data(self):
-        pass
+        self.results = []
+        self.classes = range(1, variables.classes_no + 1)
+        self.datastorage = DataStorage()
+        self.datastorage.load_data()
+        self.datastorage.sort_data()
 
     def init_classes(self):
-        pass
+        entries_amount = len(self.datastorage)
+        for i in range(entries_amount):
+            self.datastorage.data[i].dummy_value = self.get_class(i)
 
     def perform_f_tests(self):
         pass
 
     def give_output(self):
         pass
+
+    def get_class(self, i):
+        return math.floor(i / variables.classes_no) + 1
