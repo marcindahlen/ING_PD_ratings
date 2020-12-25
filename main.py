56cad4551c47197d3b1d0deccb0eca52cdb8a8e9
dahlen <marcin.dahlen@gmail.com>
2020-12-25 19:20:03 +0100

    final touches

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from source.OptiparmBasic import Optiparm
 
+"""
+Expected hierarchy:
+    root/
+        pd_model.csv
+        ING_PD_ratings/
+            main.py
+            test_datastorage.py
+            source/
+                DataEntry.py
+                DataStorage.py
+                OptiparmBasic.py
+            utils/
+                variables.py
+"""
+
 algorithm = Optiparm()
 
 algorithm.init_classes()
